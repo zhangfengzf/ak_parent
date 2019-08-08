e9b202f115516752f603c0a6e4e37471c35febb9
hejun <hejun>
2019-08-08 16:48:39 +0800

    英文文字 分段

--- ak_speechrecognition/src/main/java/com/speechrecognition/service/impl/SpeechTranscriberListenerServiceImpl.java
+++ ak_speechrecognition/src/main/java/com/speechrecognition/service/impl/SpeechTranscriberListenerServiceImpl.java
@@ -20,12 +20,12 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
- *  语音识别实现类
+ * 语音识别实现类
  */
 @Service
 public class SpeechTranscriberListenerServiceImpl implements SpeechTranscriberListenerService {
 
-   private SpeechTranscriberListener speechTranscriberListener=null;
+    private SpeechTranscriberListener speechTranscriberListener = null;
     @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
     @Autowired
@@ -33,36 +33,33 @@ public class SpeechTranscriberListenerServiceImpl implements SpeechTranscriberLi
     @Autowired
     private RequestAddress address;
     // 存放英文文字的队列
-    private static BlockingQueue<JSONObject> englishQueue  = new ArrayBlockingQueue(20);
+    private static BlockingQueue<JSONObject> englishQueue = new ArrayBlockingQueue(20);
     // 存放中文文字的队列
-    private static BlockingQueue<JSONObject>  chineseQueue  = new ArrayBlockingQueue(20);
-    private static Map<Integer,Integer> cnMap = new ConcurrentHashMap<>();
-    private static Map<Integer,String> enMap = new ConcurrentHashMap<>();
-    // 统计返回的次数
+    private static BlockingQueue<JSONObject> chineseQueue = new ArrayBlockingQueue(20);
+    private static Map<Integer, Integer> cnMap = new ConcurrentHashMap<>();
+    private static Map<Integer, Integer> enMap = new ConcurrentHashMap<>();
+    // 统计中文同一段话返回的次数
     private static int cnNum;
-    //
     private static int cnCount;
-    private static String index ="";
-    // 字母
-    private static String charIndex ="";
-    // 上一次断句，逗号所在的位置
-    private static int lastIndex = 0;
+    private static String charIndexCn = ""; // id + 字母形式返回
+    private static int lastIndexCn = 0;   // 返回的id，逗号存在的位置
     private static int enNum;
-   // @Autowired
-  //  private HttpClientUtil httpClientUtil;
+    private static int enCount;
+    private static String charIndexEn = "";
+    private static int lastIndexEn = 0;
+
+
+    // @Autowired
+    //  private HttpClientUtil httpClientUtil;
     private static final Logger logger = LoggerFactory.getLogger(SpeechTranscriberListenerServiceImpl.class);
 
-   @Override
-    public SpeechTranscriberListener getSpeechTranscriberListener(String isOpen4G,String userId,String language,String soundCarName) {
+    @Override
+    public SpeechTranscriberListener getSpeechTranscriberListener(String isOpen4G, String userId, String language, String soundCarName) {
 
-          speechTranscriberListener = new SpeechTranscriberListener() {
-           int count =0;
-           int num =0;
+        speechTranscriberListener = new SpeechTranscriberListener() {
+            int count = 0;
+            int num = 0;
 
-           /**
-             *  语音识别
-             * @param speechTranscriberResponse
-             */
             @Override
             public void onTranscriberStart(SpeechTranscriberResponse speechTranscriberResponse) {
 
@@ -74,7 +71,7 @@ public class SpeechTranscriberListenerServiceImpl implements SpeechTranscriberLi
             }
 
             /**
-             * //识别出一句话.服务端会智能断句,当识别到一句话结束时会返回此消息
+             * 识别出一句话.服务端会智能断句,当识别到一句话结束时会返回此消息
              * @param speechTranscriberResponse
              */
             @Override
@@ -82,14 +79,14 @@ public class SpeechTranscriberListenerServiceImpl implements SpeechTranscriberLi
                 String transSentenceText = speechTranscriberResponse.getTransSentenceText();
                 JSONObject jsonObject = nextLineText(language, speechTranscriberResponse);
 
-                // 每9句话进行换行
+                // 每返回9个不同id的句子进行换行，文字保存成word时，方便用户观看！
                 count++;
-                if (count ==9){
-                    transSentenceText  +=  System.getProperty("line.separator")+System.getProperty("line.separator");
-                    count =0;
+                if (count == 9) {
+                    transSentenceText += System.getProperty("line.separator") + System.getProperty("line.separator");
+                    count = 0;
                 }
 
-                onSendWordToUser(jsonObject,language,userId,soundCarName,transSentenceText,isOpen4G);
+                onSendWordToUser(jsonObject, language, userId, soundCarName, transSentenceText, isOpen4G);
 
             }
 
@@ -101,12 +98,12 @@ public class SpeechTranscriberListenerServiceImpl implements SpeechTranscriberLi
                 String text = speechTranscriberResponse.getTransSentenceText();
                 JSONObject jsonObject = nextLineText(language, speechTranscriberResponse);
 
-                onSendWordToUser(jsonObject,language,userId,soundCarName,text,isOpen4G);
+                onSendWordToUser(jsonObject, language, userId, soundCarName, text, isOpen4G);
             }
 
             @Override
             public void onTranscriptionComplete(SpeechTranscriberResponse speechTranscriberResponse) {
-               // logger.info("语音识别：结束.........");
+                // logger.info("语音识别：结束.........");
             }
 
             @Override
@@ -114,128 +111,143 @@ public class SpeechTranscriberListenerServiceImpl implements SpeechTranscriberLi
                 logger.warn("语音识别：失败.....");
             }
         };
-       return speechTranscriberListener;
+        return speechTranscriberListener;
     }
-        public JSONObject nextLineText(String lan,SpeechTranscriberResponse speechTranscriberResponse) {
-            JSONObject jsonObject = null;
-            Object name = speechTranscriberResponse.header.get("name");
-            if ("CN".equals(lan)) {
-                jsonObject = new JSONObject();
 
-                Integer transSentenceIndex = speechTranscriberResponse.getTransSentenceIndex();
-                String transSentenceText = speechTranscriberResponse.getTransSentenceText();
-                logger.info(transSentenceText);
-                int length = transSentenceText.length();
-                int i = transSentenceText.lastIndexOf("，");
+    public JSONObject nextLineText(String lan, SpeechTranscriberResponse speechTranscriberResponse) {
 
-                if("SentenceEnd".equals(name)){
-                    System.out.println(lastIndex + " " + length);
-                    transSentenceText = transSentenceText.substring(lastIndex, length);
-                    jsonObject.put("id",transSentenceIndex + charIndex);
-                    jsonObject.put("text",transSentenceText);
-                    cnMap.remove(transSentenceIndex);
+        JSONObject jsonObject = new JSONObject();
 
-                }else if( cnMap.get(transSentenceIndex) == null){
-                    cnNum = 0;
-                    cnCount = 0;
-                    lastIndex =0;
-                    charIndex ="";
-                    transSentenceText = transSentenceText.substring(lastIndex, length);
-                    jsonObject.put("id",transSentenceIndex + charIndex);
-                    jsonObject.put("text",transSentenceText);
-                    cnMap.put(transSentenceIndex,lastIndex);
-                }else{
-                    cnNum ++;
-                    if(cnNum <30){
-                        transSentenceText = transSentenceText.substring(lastIndex, length);
-                        jsonObject.put("id",transSentenceIndex + charIndex);
-                        jsonObject.put("text",transSentenceText);
-                        //cnMap.put(transSentenceIndex,lastIndex);
-                    }else {
-                        if( length - i < 5){
-                            cnCount = cnCount +1;
-                            lastIndex = i+1;
-                            transSentenceText = transSentenceText.substring(lastIndex, length);
-                            charIndex =String.valueOf((char)(97+cnCount));
-
-                            jsonObject.put("id",transSentenceIndex + charIndex);
-                            jsonObject.put("text",transSentenceText);
-                            cnMap.put(transSentenceIndex,lastIndex);
-                            cnNum =0;
-
-                        }else {
-                            transSentenceText = transSentenceText.substring(lastIndex, length);
-                            jsonObject.put("id",transSentenceIndex + charIndex);
-                            jsonObject.put("text",transSentenceText);
-                        }
-
+        Object name = speechTranscriberResponse.header.get("name");
+        Integer transSentenceIndex = speechTranscriberResponse.getTransSentenceIndex();
+        String transSentenceText = speechTranscriberResponse.getTransSentenceText();
+        int length = transSentenceText.length();
+        int i = transSentenceText.lastIndexOf("，");
+        if ("CN".equals(lan)) {
+            if ("SentenceEnd".equals(name)) {
+                // SentenceEnd 事件表示服务端检测到了一句话的结束，并附带返回该句话的识别结果
+                transSentenceText = transSentenceText.substring(lastIndexCn, length);
+                cnMap.remove(transSentenceIndex);
+            } else if (cnMap.get(transSentenceIndex) == null) {
+                cnNum = 0;
+                cnCount = 0;
+                lastIndexCn = 0;
+                charIndexCn = "";
+                transSentenceText = transSentenceText.substring(lastIndexCn, length);
+                cnMap.put(transSentenceIndex, lastIndexCn);
+            } else {
+                cnNum++;
+                if (cnNum < 30) {
+                    System.out.println(lastIndexCn+"  "+ length);
+                    transSentenceText = transSentenceText.substring(lastIndexCn, length);
+                    //cnMap.put(transSentenceIndex,lastIndex);
+                } else {
+                    if (length - i < 5) {
+                        cnCount = cnCount + 1;
+                        lastIndexCn = i + 1;
+                        transSentenceText = transSentenceText.substring(lastIndexCn, length);
+                        charIndexCn = String.valueOf((char) (97 + cnCount));
+                        cnMap.put(transSentenceIndex, lastIndexCn);
+                        cnNum = 0;
+                    } else {
+                        transSentenceText = transSentenceText.substring(lastIndexCn, length);
                     }
-
                 }
-
-
-
             }
-            jsonObject.put("state",name);
-            System.out.println(jsonObject);
-                return jsonObject;
+            jsonObject.put("id", transSentenceIndex + charIndexCn);
+        }else {
+            if ("SentenceEnd".equals(name)) {
+                // SentenceEnd 事件表示服务端检测到了一句话的结束，并附带返回该句话的识别结果
+                transSentenceText = transSentenceText.substring(lastIndexEn, length);
+                cnMap.remove(transSentenceIndex);
+            } else if (cnMap.get(transSentenceIndex) == null) {
+                cnNum = 0;
+                cnCount = 0;
+                lastIndexEn = 0;
+                charIndexEn = "";
+                transSentenceText = transSentenceText.substring(lastIndexEn, length);
+                cnMap.put(transSentenceIndex, lastIndexEn);
+            } else {
+                cnNum++;
+                if (cnNum < 30) {
+                    transSentenceText = transSentenceText.substring(lastIndexEn, length);
+                    //cnMap.put(transSentenceIndex,lastIndex);
+                } else {
+                    if (length - i < 5) {
+                        cnCount = cnCount + 1;
+                        lastIndexEn = i + 1;
+                        transSentenceText = transSentenceText.substring(lastIndexEn, length);
+                        charIndexEn = String.valueOf((char) (97 + cnCount));
+                        cnMap.put(transSentenceIndex, lastIndexEn);
+                        cnNum = 0;
+                    } else {
+                        transSentenceText = transSentenceText.substring(lastIndexEn, length);
+                    }
+                }
             }
+            jsonObject.put("id", transSentenceIndex + charIndexEn);
+        }
+        jsonObject.put("text", transSentenceText);
+        jsonObject.put("state", name);
+        System.out.println(jsonObject);
+        return jsonObject;
+    }
 
-      @Async
-      public void sendMessageToWordService(String isOpen4G,String language){
-        if("open".equals(isOpen4G)){
-            logger.info("当前线程:   "+Thread.currentThread().getName()+"  4G开启，文字传输开始..........");
+    @Async
+    public void sendMessageToWordService(String isOpen4G, String language) {
+        if ("open".equals(isOpen4G)) {
+            logger.info("当前线程:   " + Thread.currentThread().getName() + "  4G开启，文字传输开始..........");
             // 切换语音或者关闭语音识别时，需要结束当前线程，跳出循环
-            while(Common.getIsOpenSpeech()&&Common.getSoundCarAndLanguageState()){
+            while (Common.getIsOpenSpeech() && Common.getSoundCarAndLanguageState()) {
                 try {
-                    if("CN".equals(language)){
-                        if(!chineseQueue.isEmpty()){
+                    if ("CN".equals(language)) {
+                        if (!chineseQueue.isEmpty()) {
                             JSONObject json = chineseQueue.take();
-                            httpUrlConnection.httpPost(address.getServerWordAddress()+"cnSound",JSONObject.toJSONString(json));
+                            httpUrlConnection.httpPost(address.getServerWordAddress() + "cnSound", JSONObject.toJSONString(json));
                         }
-                    }else{
-                        if (!englishQueue.isEmpty()){
-                            JSONObject json =englishQueue.take();
-                            httpUrlConnection.httpPost(address.getServerWordAddress()+"enSound",JSONObject.toJSONString(json));
+                    } else {
+                        if (!englishQueue.isEmpty()) {
+                            JSONObject json = englishQueue.take();
+                            httpUrlConnection.httpPost(address.getServerWordAddress() + "enSound", JSONObject.toJSONString(json));
                         }
                     }
-                }catch (InterruptedException e){
+                } catch (InterruptedException e) {
                     e.printStackTrace();
-                }catch (Exception e){
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         }
-      }
+    }
 
-       public void  onSendWordToUser(JSONObject jsonObject, String language, String userId, String soundCarName, String text, String isOpen4G){
-          String message =jsonObject.toString();
+    public void onSendWordToUser(JSONObject jsonObject, String language, String userId, String soundCarName, String text, String isOpen4G) {
+        String message = jsonObject.toString();
         // String text = speechTranscriberResponse.getTransSentenceText();
-         // 将文字发送给局域网的用户
-         simpMessagingTemplate.convertAndSendToUser(userId,"/serverwebsoket/"+soundCarName,message );
-         // 将文字发送至服务器，用于发送通过4G网络访问的用户
-         if("open".equals(isOpen4G)){
-             putMessageQueue(message,language,userId,text);
-         }
-      }
+        // 将文字发送给局域网的用户
+        simpMessagingTemplate.convertAndSendToUser(userId, "/serverwebsoket/" + soundCarName, message);
+        // 将文字发送至服务器，用于发送通过4G网络访问的用户
+        if ("open".equals(isOpen4G)) {
+            putMessageQueue(message, language, userId, text);
+        }
+    }
 
-       public void putMessageQueue(String message,String language,String userId,String text){
+    public void putMessageQueue(String message, String language, String userId, String text) {
         JSONObject jsonObject = new JSONObject();
-        jsonObject.put("userId",userId);
-        jsonObject.put("jsonString",message);
-        jsonObject.put("text",text);
-          if("CN".equals(language)){
-              jsonObject.put("soundCarName","cn");
-             if(!chineseQueue.offer(jsonObject)){
-                 chineseQueue.clear();
-             }
-          }else{
-              jsonObject.put("soundCarName","en");
-             if(!englishQueue.offer(jsonObject)){
-                 englishQueue.clear();
-           }
+        jsonObject.put("userId", userId);
+        jsonObject.put("jsonString", message);
+        jsonObject.put("text", text);
+        if ("CN".equals(language)) {
+            jsonObject.put("soundCarName", "cn");
+            if (!chineseQueue.offer(jsonObject)) {
+                chineseQueue.clear();
+            }
+        } else {
+            jsonObject.put("soundCarName", "en");
+            if (!englishQueue.offer(jsonObject)) {
+                englishQueue.clear();
+            }
 
-       }
-   }
+        }
+    }
 }
 
